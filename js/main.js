1d392e718597ebebdd8952447d5d8fac600f804c
sayfullaevoloviddin <sayfullaevoloviddin73@gmail.com>
2023-05-17 22:07:23 +0500

    js 1-dars yuklandi
    js 1-darsning kodlari

---
+++ js/main.js
@@ -0,0 +1,40 @@
+/*console.log(4*4%5/4+2345423-28342*9834);*/
+
+/*alert("Saytimizga xush kelibsiz")*/
+
+/*document.write("Men body oynasida chiqaman.")*/
+
+/*let x = 2;*/
+/*let y = 5;*/
+/*x = 10;*/
+/*let salom = "Assalomu aleykum";*/
+/*let rang = "#f1f1f1";*/
+/*document.write(x + y);*/
+/*let x = 2, y = 5, z = 10;*/
+
+/*document.write(x + y + z);*/
+/*JS data types */
+/*let x = 5;*/
+/*let soz = "Javascript"*/
+/*document.write(typeof soz);*/
+
+/* JSda arifmetik amallar */
+let x = 50;
+/*x+=1;*/
+/*x-=2;*/
+/*x*=5;*/
+/*let qoldiq = x% 5;*/
+/*let natija = x**5;*/
+/* ++x */
+/* --x */
+/* x++ */
+/* x++ */
+/*document.write(x);*/
+let son = 5;
+let matnson = "55";
+document.write(matnson/5)
+
+
+
+
+
